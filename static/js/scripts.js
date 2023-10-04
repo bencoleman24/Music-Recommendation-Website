1a24bbcfb089e923d38e336bc0a53a865822da68
Ben Coleman <97268624+bencoleman24@users.noreply.github.com>
2023-10-03 23:50:27 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -36,12 +36,19 @@ $(document).ready(function() {
             $('#recommendationsList').empty();
             if (data.status === "success" && data.data && data.data.length && data.links && data.links.length === data.data.length) {
                 for(let i = 0; i < data.data.length; i++) {
-                    $('#recommendationsList').append('<li class="list-group-item">' + data.data[i] + ' - <a href="' + data.links[i] + '" target="_blank">Link</a></li>');
+                    $('#recommendationsList').append('<li class="list-group-item">' + data.data[i] + ' - <a href="' + data.links[i] + '" target="_blank">Song Link</a></li>');
                 }
                 $('#resultsSection').show();
+                
+                $('#recommendationTitle').show();
+                
                 $('#feedbackSection').show();
 
                 $('#generateBtn').hide();
+                
+                // Re-enable the "Submit Feedback" button when new recommendations are displayed
+                $('#submitFeedback').prop('disabled', false);
+
             } else {
                 alert(data.message);  
             }
@@ -50,6 +57,7 @@ $(document).ready(function() {
 
     $('#restartButton').click(function() {
         $('#resultsSection').hide();
+        $('#recommendationTitle').hide();
         $('#feedbackSection').hide();
         $('#initialPrompt').show();
 
@@ -57,36 +65,36 @@ $(document).ready(function() {
     });
 
     $('#submitFeedback').click(function() {
-    let feedback = $('#feedbackRating').val();
-    let form_data = $("form").serializeArray();
-    let input_data = "";
-    form_data.forEach(item => {
-        if(item.name === "artist" && item.value !== "") {
-            input_data += "Artist: " + item.value + ", ";
-        }
-        if(item.name === "track_name" && item.value !== "") {
-            input_data += "Track: " + item.value;
-        }
-    });
-    let model = $('#modelTypeSelector').val();
-    let output = $('#recommendationsList').text();  
+        let feedback = $('#feedbackRating').val();
+        let form_data = $("form").serializeArray();
+        let input_data = "";
+        form_data.forEach(item => {
+            if(item.name === "artist" && item.value !== "") {
+                input_data += "Artist: " + item.value + ", ";
+            }
+            if(item.name === "track_name" && item.value !== "") {
+                input_data += "Track: " + item.value;
+            }
+        });
+        let model = $('#modelTypeSelector').val();
+        let output = $('#recommendationsList').text();  
 
-    $.post("/submit_feedback", {
-        feedback: feedback,
-        input_data: input_data,
-        model: model,
-        output: output
-    }, function(data) {
-        if (data.status === "success") {
-            $('#feedbackSubmittedMessage').show(500);
-            setTimeout(function() {
-                $('#feedbackSubmittedMessage').hide();
-            }, 5000);
-            $('#submitFeedback').prop('disabled', true);  
-        } else {
-            alert(data.message);
-        }
+        $.post("/submit_feedback", {
+            feedback: feedback,
+            input_data: input_data,
+            model: model,
+            output: output
+        }, function(data) {
+            if (data.status === "success") {
+                $('#feedbackSubmittedMessage').show(500);
+                setTimeout(function() {
+                    $('#feedbackSubmittedMessage').hide();
+                }, 5000);
+                $('#submitFeedback').prop('disabled', true);  
+            } else {
+                alert(data.message);
+            }
+        });
     });
-});
 
 });
